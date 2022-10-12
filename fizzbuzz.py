817bf2e96aa8427d98a3bc4db556b7619edb1244
CheeseFan40 <89537997+CheeseFan40@users.noreply.github.com>
2022-10-11 22:22:56 -0500

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,13 @@
-for x in range(0,101):
+for x in range(101):
+    ##multiples of 15
     if (x%15 == 0):
         print("FizzBuzz")
         continue
+    #multiples of 5
     elif (x%5 == 0):
         print("Buzz")
         continue
+    ##multiples of 3
     elif (x%3 == 0):
         print("Fizz")
         continue
