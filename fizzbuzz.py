7094a560e4ad3115280fbca4316acb04b6deee60
CheeseFan40 <89537997+CheeseFan40@users.noreply.github.com>
2022-10-11 22:19:17 -0500

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+for x in range(0,101):
+    if (x%15 == 0):
+        print("FizzBuzz")
+        continue
+    elif (x%5 == 0):
+        print("Buzz")
+        continue
+    elif (x%3 == 0):
+        print("Fizz")
+        continue
+    print(x)
